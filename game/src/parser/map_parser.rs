6528558d23306a5280534e5bc3080cdf3a95313a
K K <kamil2005kai@gmail.com>
2026-02-04 21:42:30 +0100

    adding poylgon fuction (polygon export form commands)

--- game/src/parser/map_parser.rs
+++ game/src/parser/map_parser.rs
@@ -1,7 +1,7 @@
 //use Geogebra API to parse map from Geogebra
 
 use anyhow::{Context, Result};
-use quick_xml::Reader;
+use quick_xml::{Reader, name};
 use quick_xml::events::Event;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -31,40 +31,55 @@ fn reading_attr_from_ggb(path: &str) -> Result<()> {
      let mut pointList: Vec<GeogebraPoint> = Vec::new();
 
     loop {
-        match reader.read_event_into(&mut buf)? {
-            Event::Start(ref e) if e.name().as_ref() == b"element" => {
-                let mut element_type = None;
-                let mut label = "unnamed".to_string();
+    match reader.read_event_into(&mut buf)? {
+        Event::Start(ref e) if e.name().as_ref() == b"element" => {
+            let mut element_type = None;
+            let mut label = "unnamed".to_string();
 
-                for attr in e.attributes() {
-                    let attr = attr?;
-                    match attr.key.as_ref() {
-                        b"type" => element_type = Some(attr.unescape_value()?.to_string()),
-                        b"label" => label = attr.unescape_value()?.to_string(),
-                        _ => {}
-                    }
-                }
-
-                match element_type.as_deref() {
-                    Some("point") => read_point(&mut reader, &mut buf, &label,&mut pointList)?,
-                    Some("segment") => read_segment(&mut reader, &mut buf, &label)?,
+            for attr in e.attributes() {
+                let attr = attr?;
+                match attr.key.as_ref() {
+                    b"type" => element_type = Some(attr.unescape_value()?.to_string()),
+                    b"label" => label = attr.unescape_value()?.to_string(),
                     _ => {}
                 }
             }
-            Event::Eof => break,
-            _ => {}
+
+            match element_type.as_deref() {
+                Some("point") => read_point(&mut reader, &mut buf, &label, &mut pointList)?,
+                Some("segment") => read_segment(&mut reader, &mut buf, &label)?,
+                _ => {}
+            }
         }
-        //TODO
-        // match reader.read_event_into(&mut buf)? {
-        //     Event::Start(ref e) if e.name().as_ref() == b"command" => {
-        //     _ => {
-        // }
-        //TODO END
-        buf.clear();
+
+        Event::Start(ref e) if e.name().as_ref() == b"command" => {
+            let mut command_name = None;
+
+            for attr in e.attributes() {
+                let attr = attr?;
+                if attr.key.as_ref() == b"name" {
+                    command_name = Some(attr.unescape_value()?.to_string());
+                }
+            }
+
+            match command_name.as_deref() {
+                Some("Polygon") => {
+                    println!("Found Polygon Command");
+                    read_polygon(&mut reader, &mut buf, "Polygon")?;    
+                }
+                _ => {}
+            }
+        }
+
+        Event::Eof => break,
+        _ => {}
     }
 
+    buf.clear();
+}
     Ok(())
 }
+
 
 fn read_point(reader: &mut Reader<&[u8]>, buf: &mut Vec<u8>, name: &str, pointList: &mut Vec<GeogebraPoint>) -> Result<()> {
     let mut x = None;
@@ -136,5 +151,36 @@ fn read_segment(reader: &mut Reader<&[u8]>, buf: &mut Vec<u8>, name: &str) -> Re
 
         buf.clear();
     }
+    Ok(())
+}
+
+fn read_polygon(reader: &mut Reader<&[u8]>, buf: &mut Vec<u8>, name: &str) -> Result<()> {
+    let mut name = name.to_string();
+    let mut vertices: Vec<String> = Vec::new();
+    loop {
+        match reader.read_event_into(buf)? {
+            Event::Empty(ref e) if e.name().as_ref() == b"input" => {
+                for attr in e.attributes() {
+                    let attr = attr?;
+                    vertices.push(attr.unescape_value()?.to_string());
+                }
+            }
+
+            Event::End(ref e) if e.name().as_ref() == b"command" => {
+                break;
+            }
+
+            _ => {}
+        }
+        // //todo
+        // match reader.read_event_into(buf)? {
+        //     Event::End(ref e) if e.name().as_ref() == b"output" => break,
+        //     _ => {}
+        // }
+        // //todo end
+        buf.clear();
+    }
+
+    println!("Polygon: {}, Vertices: {:?}", name, vertices);
     Ok(())
 }
