a6277e647740b4b7d0c101dbac39b0fbfbf1d145
Keith1765 <i.dretser@campus.lmu.de>
2026-02-12 18:21:32 +0100

    changed step up to foot lvl and improved slide

--- game/src/game/player.rs
+++ game/src/game/player.rs
@@ -8,9 +8,10 @@ use crate::{
 };
 use minifb::{Key, MouseMode, Window};
 use std::f64::consts::PI;
-use crate::game::player::Slide_Direction::*;
+use crate::game::player::LastInputDirection::*;
 
-const ROTATION_SPEED: f64 = 2.0;
+const ROTATION_SPEED_MOUSE: f64 = 2.0;
+const ROTATION_SPEED_KEYS: f64 = 0.1;
 pub const MOVE_SPEED: f64 = 1.0;
 const FLY_UP_DOWN_SPEED: f64 = 1.0;
 const MOVEMENT_SMOOTHING_SPEED: f64 = 1.5;
@@ -20,9 +21,10 @@ pub const PLAYER_VIEW_HEIGHT: f64 = 15.0;
 const SPRINT_SPEED: f64 = 3.0;
 const CROUCH_Distance: f64 = 7.5; 
 const SLIDE_COOLDOWN_TIME: i32 = 15;
+const STRAIFING_SPEED: f64 = 0.02;
 
 #[derive(Clone,PartialEq, Eq)]
-pub enum Slide_Direction {
+pub enum LastInputDirection {
     W,
     A,
     S,
@@ -38,8 +40,10 @@ pub struct Player {
     pub godmode: bool,
     pub move_speed: f64,
     pub is_sliding: bool,
-    pub slide_dir: Slide_Direction,
+    pub last_input: LastInputDirection,
     pub slice_cooldown: i32,
+    pub is_jumping: bool,
+    
 }
 
 impl Player {
@@ -55,14 +59,15 @@ impl Player {
                 facing_direction: pa,
                 
             },
-            velocity_x: pa.cos() * ROTATION_SPEED,
-            velocity_y: pa.sin() * ROTATION_SPEED,
+            velocity_x: pa.cos() * ROTATION_SPEED_MOUSE,
+            velocity_y: pa.sin() * ROTATION_SPEED_MOUSE,
             last_mouse_x: SCREEN_WIDTH as f32 / 2.0,
             godmode: false, // allows flying up and down, no collision (when those are implemented)
             move_speed: 1.0,
             is_sliding: false,
-            slide_dir: No,
+            last_input: No,
             slice_cooldown: 0,
+            is_jumping: false,
         }
     }
 
@@ -75,33 +80,51 @@ impl Player {
             self.last_mouse_x = mx; // store for next frame
             self.update_dir();
         }
-        if window.is_key_down(Key::Q) {
+
+        if window.is_key_down(Key::Left) {
+            //during slide heavily restricted rotation
+            let rotation_factor = match self.is_sliding {
+                true => STRAIFING_SPEED,
+                false => ROTATION_SPEED_KEYS,
+            };
+
             self.check_angle();
-            self.mover.facing_direction -= 0.1;
+            self.mover.facing_direction -= rotation_factor;
             self.update_dir();
         }
 
-        if window.is_key_down(Key::Left) && !self.is_sliding {
+        if window.is_key_down(Key::Right) {
+            //during slide heavily restricted rotation
+            let rotation_factor = match self.is_sliding {
+                true => STRAIFING_SPEED,
+                false => ROTATION_SPEED_KEYS,
+            };
             self.check_angle();
-            self.mover.facing_direction -= 0.1;
+            self.mover.facing_direction += rotation_factor;
             self.update_dir();
         }
 
-        if window.is_key_down(Key::E) {
-            self.check_angle();
-            self.mover.facing_direction += 0.1;
-            self.update_dir();
-        }
-
-        if window.is_key_down(Key::Right) && !self.is_sliding {
-            self.check_angle();
-            self.mover.facing_direction += 0.1;
-            self.update_dir();
-        }
-
-        if window.is_key_down(Key::W) && (!self.is_sliding || self.slide_dir==W) {
+        if window.is_key_down(Key::W) && ((!self.is_sliding && !self.is_jumping) || self.last_input==W) {
             self.mover.step(self.move_speed, 0.0, map, self.godmode);
             
+        }
+        if window.is_key_down(Key::A) && ((!self.is_sliding && !self.is_jumping)|| self.last_input==A) {
+            self.mover.step(self.move_speed, -PI / 2.0, map, self.godmode);
+        }
+        if window.is_key_down(Key::D) && ((!self.is_sliding && !self.is_jumping) || self.last_input==D){
+            self.mover.step(self.move_speed, PI / 2.0, map, self.godmode);
+        }
+
+        if window.is_key_down(Key::S) && ((!self.is_sliding && !self.is_jumping) || self.last_input==S) {
+            self.mover.step(self.move_speed, PI, map, self.godmode);
+        }
+
+        if window.is_key_down(Key::Space) && self.godmode {
+            self.mover.foot_level += FLY_UP_DOWN_SPEED;
+        }
+
+        if window.is_key_down(Key::LeftShift) && self.godmode {
+            self.mover.foot_level -= FLY_UP_DOWN_SPEED;
         }
 
         //slowdown movespeed if not sprinting and sliding anymore
@@ -122,7 +145,8 @@ impl Player {
                 self.move_speed = SPRINT_SPEED;
             }
         }
-        if self.slice_cooldown > 0 {
+
+        if (self.slice_cooldown > 0) && !window.is_key_down(Key::Down) {
             self.slice_cooldown -= 1;
         } 
         //init slide gives speed boost
@@ -131,10 +155,10 @@ impl Player {
                 self.move_speed += 5.0;
                 self.is_sliding = true;
                 self.mover.foot_level -=CROUCH_Distance;
-                if window.is_key_down(Key::D) {self.slide_dir = D};
-                if window.is_key_down(Key::A) {self.slide_dir = A};
-                if window.is_key_down(Key::S) {self.slide_dir = S};
-                if window.is_key_down(Key::W) {self.slide_dir = W};
+                if window.is_key_down(Key::D) {self.last_input = D};
+                if window.is_key_down(Key::A) {self.last_input = A};
+                if window.is_key_down(Key::S) {self.last_input = S};
+                if window.is_key_down(Key::W) {self.last_input = W};
             }
             
         }
@@ -144,32 +168,15 @@ impl Player {
             self.move_speed -= 0.2;
         }
 
-        //ending slide
-        if !window.is_key_down(Key::Down) && self.is_sliding{
+        //ending slide (either cause not pressed or slowed down)
+        if (!window.is_key_down(Key::Down) && self.is_sliding) || ((self.move_speed <= SPRINT_SPEED) && self.is_sliding){
             self.is_sliding = false;
             self.mover.foot_level +=CROUCH_Distance;
-            self.slide_dir=No;
+            self.last_input=No;
             self.slice_cooldown = SLIDE_COOLDOWN_TIME;
         }
 
-        if window.is_key_down(Key::A) && (!self.is_sliding || self.slide_dir==A) {
-            self.mover.step(self.move_speed, -PI / 2.0, map, self.godmode);
-        }
-        if window.is_key_down(Key::D) && (!self.is_sliding || self.slide_dir==D){
-            self.mover.step(self.move_speed, PI / 2.0, map, self.godmode);
-        }
-
-        if window.is_key_down(Key::S) && (!self.is_sliding || self.slide_dir==S) {
-            self.mover.step(self.move_speed, PI, map, self.godmode);
-        }
-
-        if window.is_key_down(Key::Space) && self.godmode {
-            self.mover.foot_level += FLY_UP_DOWN_SPEED;
-        }
-
-        if window.is_key_down(Key::LeftShift) && self.godmode {
-            self.mover.foot_level -= FLY_UP_DOWN_SPEED;
-        }
+        
 
         // TODO make not fiddly (currently switches every tick)
         if window.is_key_down(Key::G) {
@@ -200,7 +207,7 @@ impl Player {
     }
 
     fn update_dir(&mut self) {
-        self.velocity_x = self.mover.facing_direction.cos() * ROTATION_SPEED;
-        self.velocity_y = self.mover.facing_direction.sin() * ROTATION_SPEED;
+        self.velocity_x = self.mover.facing_direction.cos() * ROTATION_SPEED_MOUSE;
+        self.velocity_y = self.mover.facing_direction.sin() * ROTATION_SPEED_MOUSE;
     }
 }
