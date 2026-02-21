60e8dfad8635c0ca8c19b54b0281f5b8f5d91427
K K <kamil2005kai@gmail.com>
2026-02-21 18:13:48 +0100

    Refactor interactables parser for improved readability and add a part of button handling logic

--- game/src/parser/interactables_parser.rs
+++ game/src/parser/interactables_parser.rs
@@ -1,6 +1,6 @@
+use crate::game::interactables::*;
 use crate::game::map::Point;
 use crate::render::RendererData;
-use crate::game::interactables::*;
 
 use anyhow::Result;
 use quick_xml::Reader;
@@ -11,11 +11,17 @@ use std::io::Read;
 
 use crate::parser::map_parser::SCALING_FACTOR;
 
-pub fn parse_interactables(path: String, renderer_data: &RendererData) -> Result<Vec<Interactable>> {
+pub fn parse_interactables(
+    path: String,
+    renderer_data: &RendererData,
+) -> Result<Vec<Interactable>> {
     read_interactables_from_file(path, renderer_data)
 }
 
-pub fn read_interactables_from_file(path: String, renderer_data: &RendererData) -> Result<Vec<Interactable>> {
+pub fn read_interactables_from_file(
+    path: String,
+    renderer_data: &RendererData,
+) -> Result<Vec<Interactable>> {
     let mut file = File::open(path)?;
     let mut xml_contents = String::new();
     file.read_to_string(&mut xml_contents)?;
@@ -23,8 +29,7 @@ pub fn read_interactables_from_file(path: String, renderer_data: &RendererData) 
     let mut reader = Reader::from_str(&xml_contents);
     let mut buf = Vec::new();
 
-    let mut Interactable
-    : Vec<Interactable> = Vec::new();
+    let mut Interactable: Vec<Interactable> = Vec::new();
 
     loop {
         match reader.read_event_into(&mut buf)? {
@@ -40,13 +45,21 @@ pub fn read_interactables_from_file(path: String, renderer_data: &RendererData) 
                         _ => {}
                     }
                 }
+                let interactable_string_type = split_string_uppercase(label.as_str());
+                if interactable_string_type[0] == "Interactable" {
+                    match (interactable_string_type[1].as_str()) {
+                        //todo
+                        "Button" => read_values_for_button(
+                            &mut reader,
+                            &mut buf,
+                            &mut Interactable,
+                            renderer_data,
+                            interactable_string_type.clone(),
+                        )?,
+                        "Elevator" => {}
 
-                match (element_type.as_deref(), label.as_str()) {
-                    (Some("point"), label) if label.starts_with("Interactable") => {
-                        read_values_for_enemy_from_point(&mut reader, &mut buf, &mut Interactable
-                            , renderer_data)?
+                        _ => {}
                     }
-                    _ => {}
                 }
             }
             Event::Eof => break,
@@ -55,14 +68,12 @@ pub fn read_interactables_from_file(path: String, renderer_data: &RendererData) 
 
         buf.clear();
     }
-    Ok(Interactable
-    )
+    Ok(Interactable)
 }
 fn read_values_for_enemy_from_point(
     reader: &mut Reader<&[u8]>,
     buf: &mut Vec<u8>,
-    Interactable
-    : &mut Vec<Interactable>,
+    Interactable: &mut Vec<Interactable>,
     renderer_data: &RendererData,
 ) -> Result<()> {
     let mut x = None;
@@ -101,4 +112,71 @@ fn read_values_for_enemy_from_point(
         buf.clear();
     }
     Ok(())
+}
+fn read_values_for_button(
+    reader: &mut Reader<&[u8]>,
+    buf: &mut Vec<u8>,
+    Interactable: &mut Vec<Interactable>,
+    renderer_data: &RendererData,
+    interactable_string_type: Vec<String>,
+) -> Result<()> {
+    let mut x = None;
+    let mut y = None;
+    let mut floor_level = None;
+    let mut facing_direction = None;
+    let mut enemy_type_num = None;
+    loop {
+        match reader.read_event_into(buf)? {
+            Event::Empty(ref e) if e.name().as_ref() == b"coords" => {
+                for attr in e.attributes() {
+                    let attr = attr?;
+                    match attr.key.as_ref() {
+                        b"x" => x = Some(attr.unescape_value()?.parse::<f64>()?),
+                        b"y" => y = Some(attr.unescape_value()?.parse::<f64>()?),
+                        _ => {}
+                    }
+                }
+            }
+            Event::Empty(ref e) if e.name().as_ref() == b"objColor" => {
+                for attr in e.attributes() {
+                    let attr = attr?;
+
+                    match attr.key.as_ref() {
+                        b"r" => floor_level = Some(attr.unescape_value()?.parse::<f64>()?),
+                        b"g" => facing_direction = Some(attr.unescape_value()?.parse::<f64>()?),
+                        b"b" => enemy_type_num = Some(attr.unescape_value()?.parse::<i32>()?),
+                        //b"alpha" => idk = Some(attr.unescape_value()?.parse::<u8>()?), //left for later use
+                        _ => {}
+                    }
+                }
+            }
+            Event::End(ref e) if e.name().as_ref() == b"element" => break,
+            _ => {}
+        }
+        buf.clear();
+    }
+    match interactable_string_type[2].as_str() {
+        "Map" => {}
+        "Spawner" => {}
+        _ => {}
+    }
+    Ok(())
+}
+
+pub fn split_string_uppercase(input: &str) -> Vec<String> {
+    let mut parts = Vec::new();
+    let mut current = String::new();
+
+    for c in input.chars() {
+        if c.is_uppercase() && !current.is_empty() {
+            parts.push(current);
+            current = String::new();
+        }
+        current.push(c);
+    }
+
+    if !current.is_empty() {
+        parts.push(current);
+    }
+    parts
 }
