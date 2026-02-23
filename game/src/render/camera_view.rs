d743b9588f341a770d19d1349e42f909adc28703
JakobGasteiger <jakob@gasteiger.online>
2026-02-23 23:53:46 +0100

    fixed interactable and entity rendering properly this time

--- game/src/render/camera_view.rs
+++ game/src/render/camera_view.rs
@@ -151,12 +151,13 @@ fn draw_camera_view(buffer: &mut [u32], renderer_data: &RendererData, game: &Gam
     // create entity (sprite) tasks, put them into the taskings
     for entity in &game.entities {
         if let Some(mut instruction) = task_sprite(game, &entity.sprite, &entity.mover, renderer_data) {
-            for x in instruction.sprite_left_screen_x..instruction.sprite_right_screen_x {
+            let sprite_width = instruction.sprite_right_screen_x - instruction.sprite_left_screen_x;
+            for x in 0..sprite_width {
                 if x < 0 || x > SCREEN_WIDTH - 1 {
                     continue;
                 }
 
-                if let Some(cts) = &mut columns_tasked[x]
+                if let Some(cts) = &mut columns_tasked[instruction.sprite_right_screen_x-x-1]
                     && let Some(sprite_task) =
                         instruction.tasks.pop()
                     && sprite_task.distance <= cts.wall_distance
@@ -171,12 +172,13 @@ fn draw_camera_view(buffer: &mut [u32], renderer_data: &RendererData, game: &Gam
     // practically identical to above
     for interactable in &game.interactables {
         if let Some(mut instruction) = task_sprite(game, &interactable.sprite, &interactable.mover, renderer_data) {
-            for x in instruction.sprite_left_screen_x..instruction.sprite_right_screen_x {
+            let sprite_width = instruction.sprite_right_screen_x - instruction.sprite_left_screen_x;
+            for x in 0..sprite_width {
                 if x < 0 || x > SCREEN_WIDTH - 1 {
                     continue;
                 }
 
-                if let Some(cts) = &mut columns_tasked[x]
+                if let Some(cts) = &mut columns_tasked[instruction.sprite_right_screen_x-x-1]
                     && let Some(sprite_task) =
                         instruction.tasks.pop()
                     && sprite_task.distance <= cts.wall_distance
