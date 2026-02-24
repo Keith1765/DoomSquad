5e7914b8b03958522503f84bb1cdd307a787a6c8
Keith1765 <i.dretser@campus.lmu.de>
2026-02-24 14:56:18 +0100

    fixed clamping error

--- game/src/render/player_hp_bar.rs
+++ game/src/render/player_hp_bar.rs
@@ -2,11 +2,12 @@ use crate::{SCREEN_WIDTH, render::RendererData};
 
 
 pub fn draw_player_hp_bar (buffer: &mut [u32], render_data: & RendererData, hp: f64) {
-    let clamped_hp = hp.clamp(0.0, render_data.screen_width_as_f64) as usize;
+    // *2 multipllicator to make hp bar bigger
+    let clamped_hp = (hp * 2.0).clamp(0.0, (render_data.screen_width_as_f64) - 100.0) as usize;
 
     for hp_bar_height in 0..5 {
-        for hp_bat_length in 0..clamped_hp*2{
-            buffer[(hp_bar_height + 20)*SCREEN_WIDTH + (hp_bat_length + 50)] = 0xFF0000;
+        for hp_bar_length in 0..clamped_hp{
+            buffer[(hp_bar_height + 20)*SCREEN_WIDTH + (hp_bar_length + 50)] = 0xFF0000;
         }
     }
 
