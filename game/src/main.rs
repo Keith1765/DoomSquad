b2a3fe000dd8c1f0d477e5d0d3e2631ff42f0f0d
JakobGasteiger <jakob@gasteiger.online>
2026-02-26 21:42:43 +0100

    commented out old code

--- game/src/main.rs
+++ game/src/main.rs
@@ -25,7 +25,7 @@ const DISTANCE_DARKNESS_COEFFICIENT: f64 = 0.005;
 const WALL_DEFAULT_COLOR: u32 = 0x00ff00;
 const BLOCK_DEFAULT_COLOR: u32 = 0x0000ff;
 const SURFACE_DEFAULT_COLOR: u32 = 0xffff00;
-const AUDIO_ENABLED: bool = false; 
+const AUDIO_ENABLED: bool = true; 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     //I commented game init to test parser first
@@ -81,20 +81,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     while window.is_open() && !window.is_key_down(Key::Escape) && (game.player.hp > 0.0) {
         let (_, _, _, _, prev_space) = prev_keys;
 
-        let cur_w = window.is_key_down(Key::W);
-        let cur_a = window.is_key_down(Key::A);
-        let cur_s = window.is_key_down(Key::S);
-        let cur_d = window.is_key_down(Key::D);
-        let cur_space = window.is_key_down(Key::Space);
+        // let cur_w = window.is_key_down(Key::W);
+        // let cur_a = window.is_key_down(Key::A);
+        // let cur_s = window.is_key_down(Key::S);
+        // let cur_d = window.is_key_down(Key::D);
+        // let cur_space = window.is_key_down(Key::Space);
 
-        if let Some(a) = &mut audio {
-            let is_moving = cur_w || cur_a || cur_s || cur_d;
-            let just_jumped = cur_space && !prev_space;
+        // if let Some(a) = &mut audio {
+        //     let is_moving = cur_w || cur_a || cur_s || cur_d;
+        //     let just_jumped = cur_space && !prev_space;
             
-            a.handle_input(is_moving, just_jumped);
-        }
+        //     a.handle_input(is_moving, just_jumped);
+        // }
 
-        prev_keys = (cur_w, cur_a, cur_s, cur_d, cur_space);
+        // prev_keys = (cur_w, cur_a, cur_s, cur_d, cur_space);
 
         game.update(&window, &renderer_data);
         render::draw_screen(&mut buffer, &renderer_data, &game);
