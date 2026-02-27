069e43fce8163f873ad4a48253052178012c05e8
Keith1765 <i.dretser@campus.lmu.de>
2026-02-27 20:19:26 +0100

    implemented aim mode with lower sense that u can toggle with e and is indicated by cyan color crosshair

--- game/src/render/crosshair.rs
+++ game/src/render/crosshair.rs
@@ -3,7 +3,12 @@ use crate::{SCREEN_HEIGHT, SCREEN_WIDTH, game::map::Point, render::RendererData}
 const CROSSHAIR_THICKNESS: usize = 2;
 const CROSSHAIR_SIZE: isize = 20;
 
-pub fn draw_crosshair (buffer: &mut [u32], vertical_aim_offset: f64, renderer_data: &RendererData) {
+pub fn draw_crosshair (buffer: &mut [u32], vertical_aim_offset: f64, renderer_data: &RendererData, aim_mode: bool) {
+
+    let crosshair_color = match aim_mode {
+        true => 0x10e8e8,
+        false => 0xFFFFFF,
+    };
 
     let crosshair_vertical_offset = aim_offset_conversion(vertical_aim_offset, renderer_data);
 
@@ -11,14 +16,14 @@ pub fn draw_crosshair (buffer: &mut [u32], vertical_aim_offset: f64, renderer_da
     //draw horizontal line
     for x in -CROSSHAIR_SIZE/2..=CROSSHAIR_SIZE/2{
         for y in 0..CROSSHAIR_THICKNESS {
-            buffer[((middle_point.y + crosshair_vertical_offset)as usize + y)*SCREEN_WIDTH + (middle_point.x as isize + x) as usize] = 0xFFFFFF;
+            buffer[((middle_point.y + crosshair_vertical_offset)as usize + y)*SCREEN_WIDTH + (middle_point.x as isize + x) as usize] = crosshair_color;
         }
     }
 
     //draw vertical line
     for y in -CROSSHAIR_SIZE/2..=CROSSHAIR_SIZE/2{
         for x in 0..CROSSHAIR_THICKNESS {
-            buffer[((middle_point.y + crosshair_vertical_offset)as isize + y) as usize *SCREEN_WIDTH + (middle_point.x as usize + x)] = 0xFFFFFF;
+            buffer[((middle_point.y + crosshair_vertical_offset)as isize + y) as usize *SCREEN_WIDTH + (middle_point.x as usize + x)] = crosshair_color;
         }
     }
 
